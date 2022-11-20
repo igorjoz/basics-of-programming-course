2157224179f2a5451a5c0d11a79db742c9f805d1
Igor Jozefowicz <igorjozefowicz@gmail.com>
2022-11-20 23:43:35 +0100

    [lab7] add printfTest

---
+++ lab7/printfTest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+	printf("printf test\n");
+
+	int num1 = 3;
+	int num2 = 5;
+	int num3;
+
+	//scanf("%d", &num3);
+
+	int sum = num1 + num2;
+
+	printf("%d + %d = %d", num1, num2, sum);
+
+	return 0;
+}
